e038faed5f132847db471bc1297113e55b63d41b
pyratin <pyratin@hotmail.com>
2026-01-29 12:31:03 +0530

    toggleterm.lua

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = [[<Leader>-]],
+    direction = 'float',
+    float_opts = {
+      border = 'curved',
+      width = 120,
+      height = 30,
+    },
+  },
+}
