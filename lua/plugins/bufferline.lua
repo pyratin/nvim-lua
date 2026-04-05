1d131b665bbc8cee2645b52adf75c0a887446c53
pyratin <pyratin@hotmail.com>
2026-04-05 11:15:05 +0530

    bufferline offset foir NERDTree remove

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,21 +2,15 @@ return {
 	'akinsho/bufferline.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
-		require('bufferline').setup{
+		require('bufferline').setup({
 			options = {
 				mode = 'tabs',
 				numbers = 'ordinal',
 				show_close_icon = false,
 				show_buffer_close_icons = false,
 				diagnostics = 'nvim_lsp',
-				show_buffer_icons = true,
-				offsets = { {
-					filetype = 'nerdtree',
-					text = 'File Explorer',
-					text_align = 'left',
-					separator = true
-				} }
+				show_buffer_icons = true
 			}
-		}
+		})
 	end
 }
