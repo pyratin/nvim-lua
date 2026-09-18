79b5fb679b59d6e4b149d2b6325c089b5089202e
pyratin <pyratin@hotmail.com>
2026-09-18 15:08:55 +0530

    leap disable auto-jump

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,6 +3,11 @@ return {
 	url = 'https://git.disroot.org/andyg/leap.nvim.git',
 	pin = true,
 	config = function()
+		-- Disable autojump when there are multiple matches:
+		-- This prevents Leap from automatically jumping to the first match,
+		-- and instead shows a label (nav) for every match, including the first one.
+		require('leap').opts.safe_labels = ''
+
 		-- Set up default keymaps explicitly
 		vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
 		vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
